a428beb1dd2c97ded8181dafc7fea49dc4bcef4d
Giada196 <jadeinwonderland22@gmail.com>
2024-02-20 11:55:19 +0100

    Esercizio
    Esercizio area triangolo

---
+++ triangle.java
@@ -0,0 +1,9 @@
+public class triangle{
+    public static void main(String arg []){
+        int trianglebase= 30;
+        double triangleheight= 20.5;
+        double trianglearea= (trianglebase * triangleheight) / 2;
+
+        System.out.print("L'area del triangolo misura:" + trianglearea);
+    }
+}
