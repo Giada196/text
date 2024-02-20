2c2d4148d17fc3edff37c57ca291c7f0e361ac81
Giada196 <jadeinwonderland22@gmail.com>
2024-02-20 17:55:18 +0100

    Esercizio Funzioni- 1

---
+++ MiaClass.java
@@ -0,0 +1,11 @@
+public class MiaClass {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 30;
+        System.out.println("La somma è: " + somma(x, y));
+    }
+    public static int somma (int x, int y){
+        return x + y
+                ;
+    }
+}
