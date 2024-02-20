153e8148b0f565f674d7c02c6c1c5a4cc648c4ab
Giada196 <jadeinwonderland22@gmail.com>
2024-02-20 16:37:29 +0100

    Esercizio valore Ascii

---
+++ ValoreAscii.java
@@ -0,0 +1,17 @@
+/*public class ValoreAscii{
+    public static void main (String[] arg){
+        char c = 'P';
+        int n;
+        n= c;
+        System.out.print(n);
+    }
+}*/
+
+public class ValoreAscii{
+    public static void main(String [] arg){
+        char c = 'P' ;
+        int n;
+        n= c;
+        System.out.print(n);
+    }
+}
