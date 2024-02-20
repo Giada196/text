d8875f38c37285ccf64e221ff4b52cee1823ca8d
Giada196 <jadeinwonderland22@gmail.com>
2024-02-20 15:38:50 +0100

    Esercizio Casting Numeri

---
+++ CastingNumeri.java
@@ -0,0 +1,11 @@
+
+public class CastingNumeri{
+    public static void main(String[] arg){
+        double x = 5.8;
+        double y = 8.3;
+        int z = (int) x + (int) y;
+
+        System.out.println("La somma dei due numeri è: " + z);
+
+    }
+}
