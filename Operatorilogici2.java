80a142aaca2111063d8f669d47a856ac97e3d8c9
Giada196 <jadeinwonderland22@gmail.com>
2024-02-21 15:29:18 +0100

    Es-Operatorilogici2
    Scrivi un programma che contiene un metodo che prenda un numero in ingresso
    e capisca se un numero è pari o dispari utilizzando solo gli operatori logici.

---
+++ Operatorilogici2.java
@@ -0,0 +1,10 @@
+/* Scrivi un programma che contiene un metodo che prenda un numero in ingresso 
+e capisca se un numero è pari o dispari utilizzando solo gli operatori logici.*/
+public class Operatorilogici2 {
+    public static void main(String args[]) {
+        int x = 16;
+
+        boolean confronto = (x % 2 == 0) && (confronto = true);
+        System.out.println(confronto);
+    }
+}
