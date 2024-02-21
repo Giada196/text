a3ee58cf5491094bd6980832dcc16dc0ca000c91
Giada196 <jadeinwonderland22@gmail.com>
2024-02-21 13:14:20 +0100

    Es-Operatoricomparazione
    Scrivi un programma che contiene un metodo che che confronti due numeri interi e determini se sono diversi.
    Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza.

---
+++ Operatoricomparazione.java
@@ -0,0 +1,12 @@
+/* Scrivi un programma che contiene un metodo che che confronti due numeri interi e determini se sono diversi.
+Il programma dovrà stampare a video i due valori e il risultato dell'eguaglianza.*/
+
+public class Operatoricomparazione {
+    public static void main(String arg[]) {
+        int x = 20;
+        int y = 60;
+
+        boolean comparison = x == y;
+        System.out.println("X è uguale a Y? " + comparison);
+    }
+}
