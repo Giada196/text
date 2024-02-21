a1d5bbd19c4fc52686635511f247ffe0e2a273e4
Giada196 <jadeinwonderland22@gmail.com>
2024-02-21 23:00:21 +0100

    Es-Ciclofor2
    Scrivi un programma che contenga un metodo che prende in ingresso un numero e il massimo numero di risultati che vuoi avere e stampi tutti i numeri naturali in ordine inverso (quindi se passo il punto di partenza a 6 e gli dico di restituirmi 3 risultati mi aspetto in uscita [6 5 4] stampati).

---
+++ Ciclofor2.java
@@ -0,0 +1,10 @@
+/* Scrivi un programma che contenga un metodo che prende in ingresso un numero e il massimo numero di risultati 
+che vuoi avere e stampi tutti i numeri naturali in ordine inverso (quindi se passo il punto di partenza a 6 
+e gli dico di restituirmi 3 risultati mi aspetto in uscita [6 5 4] stampati).*/
+public class Ciclofor2 {
+    public static void main(String args[]) {
+        for (int x = 6; x >= 4; x--) {
+            System.out.println(x);
+        }
+    }
+}
