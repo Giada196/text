8f2d9aebfcadc9e685f1b1a84f9b24ff547f7066
Giada196 <jadeinwonderland22@gmail.com>
2024-02-23 12:35:42 +0100

    Update Operatorilogici2.java

--- Operatorilogici2.java
+++ Operatorilogici2.java
@@ -2,9 +2,11 @@
 e capisca se un numero è pari o dispari utilizzando solo gli operatori logici.*/
 public class Operatorilogici2 {
     public static void main(String args[]) {
-        int x = 16;
+        int x = 17;
+        System.out.println("Il valore è pari? " + paridispari(x));
+    }
 
-        boolean confronto = (x % 2 == 0) && (confronto = true);
-        System.out.println(confronto);
+    public static boolean paridispari(int x) {
+        return x % 2 == 0;
     }
 }
