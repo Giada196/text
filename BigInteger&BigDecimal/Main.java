e423c429629deef197a6720a60e5839cae2c9096
Giada196 <jadeinwonderland22@gmail.com>
2024-04-02 23:21:30 +0200

    EsBigDecimal
    Scrivi un programma Java dove imposti 2 BigDecimal e 1 operazione aritmetica (Addizione, sottrazione, moltiplicazione e divisione, min e max) definita in un enum
    Crei un metodo per calcolare l'operazione richiesta e e ritorni il risultato.
    Il tipo di operazione, i due dati in input e il risultato dovranno essere stampati a schermo

---
+++ BigInteger&BigDecimal/Main.java
@@ -0,0 +1,49 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+//BigInteger
+public class Main {
+    public static void main(String[] args){
+       BigDecimal num1 = new BigDecimal("10.5");
+       BigDecimal num2 = new BigDecimal("2.9");
+       Operazioni operazione = Operazioni.ADDIZIONE;
+       BigDecimal totale = result(num1, num2, operazione);
+        System.out.println("Numero1: " +  num1);
+        System.out.println("Numero2: " + num2);
+        System.out.println("Operazione " + operazione);
+        System.out.println("Risultato " + totale);
+
+    }
+    public static BigDecimal result (BigDecimal num1, BigDecimal num2, Operazioni operazione){
+        BigDecimal risultato = BigDecimal.ZERO;
+        switch (operazione){
+            case ADDIZIONE ->{
+                risultato = num1.add(num2);
+                 break;
+        }
+            case MOLTIPLICAZIONE -> {
+                risultato = num1.multiply(num2);
+                break;
+            }
+            case SOTTRAZIONE -> {
+                risultato = num1.subtract(num2);
+                break;
+            }
+            case DIVISIONE -> {
+                risultato = num1.divide(num2);
+                break;
+            }
+            case MAX -> {
+                risultato = num1.max(num2);
+                break;
+            }
+            case MIN -> {
+                risultato = num1.min(num2);
+                break;
+            }
+        }return risultato;
+
+    }
+
+}
