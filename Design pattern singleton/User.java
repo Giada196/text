f684e793f78d50c5751f1b2b62f8bbc27b3cb97b
Giada196 <jadeinwonderland22@gmail.com>
2024-04-05 16:37:43 +0200

    Es-DesignPatternSingleton
    Crea una classe User che abbia i parametri privati nome ed età e i getter e setter
    All'interno di User ci sarà anche una funzione che stampi a video le informazioni di età e il nome.
    Creare 2 oggetti User di cui creerà un'istanza.
    Del primo si stamperanno le informazioni di default, del secondo si cambieranno le informazioni e poi si stamperanno a video

---
+++ Design pattern singleton/User.java
@@ -0,0 +1,32 @@
+public class User {
+    private  String name;
+    private int age;
+    private static User instance;
+    private User(){}
+    public static User getInstance(){
+        if(instance == null){
+          instance = new User();
+        }
+        return instance;
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void stampaParametri(){
+        System.out.println("Nome: " + name +  "\n" + "Età: " + age);
+    }
+}
